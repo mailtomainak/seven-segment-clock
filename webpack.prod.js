2a23cfd928cf14d0e46bb648ab811e2f662826ff
mailtomainak <mailtomainak@gmail.com>
2018-11-04 23:47:57 +0530

    Add react production config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,19 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-
+const webpack = require('webpack');
 module.exports = merge(common,{
     mode:'development',
     devtool:'inline-source-map',
     devServer:{
         contentBase:'./dist'
-    }
+    },
+    optimization: {
+        minimize: true
+      },
+    plugins:[
+        new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    ]
 });
+
